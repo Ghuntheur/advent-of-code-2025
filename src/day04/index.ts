36ca0406032d66129a078496b65ebe7d5ceecea1
florian.zobele <florian.zobele@gatewatcher.com>
2025-12-05 17:03:15 +0100

    feat: day 4 - part 2

--- src/day04/index.ts
+++ src/day04/index.ts
@@ -3,7 +3,7 @@ import { join } from 'path';
 
 const input = readFileSync(
   join(import.meta.dirname, 'input.txt'),
-  'utf-8'
+  'utf-8',
 ).trim();
 
 const part1 = (input: string): number => {
@@ -49,7 +49,60 @@ const part1 = (input: string): number => {
 };
 
 const part2 = (input: string): number => {
-  return 0;
+  const grid = input.split('\n').map((line) => line.split(''));
+  const rows = grid.length;
+  const cols = grid[0].length || 0;
+
+  const countNeighbors = (col: number, row: number) => {
+    const left = grid?.[row]?.[col - 1];
+    const right = grid?.[row]?.[col + 1];
+    const top = grid?.[row - 1]?.[col];
+    const bottom = grid?.[row + 1]?.[col];
+    const topLeft = grid?.[row - 1]?.[col - 1];
+    const topRight = grid?.[row - 1]?.[col + 1];
+    const bottomLeft = grid?.[row + 1]?.[col - 1];
+    const bottomRight = grid?.[row + 1]?.[col + 1];
+
+    const all = [
+      left,
+      right,
+      top,
+      bottom,
+      topLeft,
+      topRight,
+      bottomLeft,
+      bottomRight,
+    ];
+
+    return all.filter((item) => item === '@').length;
+  };
+
+  let totalRemoved = 0;
+
+  while (true) {
+    let toRemove: { col: number; row: number }[] = [];
+
+    for (let j = 0; j < rows; j++) {
+      for (let i = 0; i < cols; i++) {
+        if (grid[j][i] === '@' && countNeighbors(i, j) < 4) {
+          toRemove.push({ col: i, row: j });
+        }
+      }
+    }
+
+    if (toRemove.length === 0) {
+      break;
+    }
+
+    for (const { col, row } of toRemove) {
+      grid[row][col] = '.';
+      totalRemoved++;
+    }
+
+    toRemove = [];
+  }
+
+  return totalRemoved;
 };
 
 console.log('Part 1:', part1(input));
